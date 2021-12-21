b822edad2bb2dd173c579219ebe2dbe3b3e0aefc
xlassix <46370698+xlassix@users.noreply.github.com>
2021-12-21 10:30:17 +0100

    done

--- src/api/purchase_order/purchase_order.services.js
+++ src/api/purchase_order/purchase_order.services.js
@@ -1,8 +1,7 @@
 import { PurchaseOrder } from './purchase_order.model'
-import { merge,differenceBy,unionBy } from 'lodash'
-import mongoose from "mongoose"
+import { merge, differenceBy, unionBy } from 'lodash'
+import mongoose from 'mongoose'
 import assert from 'assert'
-
 
 export const createPurchaseorderService = async (data) => {
   data['related_items'] = data['items']
@@ -21,21 +20,30 @@ export const invoicePurchaseorderService = async (id, amount) => {
     .exec()
 }
 
-
 export const editPurchaseorderService = async (data) => {
-  data["related_items"]=data["items"].map(elem=>{
-    return {...elem,_id:elem.id}
+  data['related_items'] = data['items'].map((elem) => {
+    return { ...elem, _id: elem.id }
   })
-  const db_data=await PurchaseOrder.findById(data.id).populate("related_items")
+  const db_data = await PurchaseOrder.findById(data.id)
+    .populate('related_items')
     .lean()
     .exec()
-  assert.notEqual(db_data, null,"couldnt find elem with matching Id");
-  db_data["related_items"]=db_data["related_items"].map(elem=>{
-    elem._id=elem._id.toString()
+  assert.notEqual(db_data, null, 'couldnt find elem with matching Id')
+  db_data['related_items'] = db_data['related_items'].map((elem) => {
+    elem._id = elem._id.toString()
     return elem
   })
-  assert.equal(differenceBy(data["related_items"], db_data["related_items"],"_id").length,0,"item are not present in this order")
-  data["related_items"]=(unionBy(db_data["related_items"],data["related_items"],"_id"))
-  return await PurchaseOrder.findByIdAndUpdate(data.id,data,{new:true}).lean()
-  .exec()
+  assert.equal(
+    differenceBy(data['related_items'], db_data['related_items'], '_id').length,
+    0,
+    'item are not present in this order'
+  )
+  data['related_items'] = unionBy(
+    db_data['related_items'],
+    data['related_items'],
+    '_id'
+  )
+  return await PurchaseOrder.findByIdAndUpdate(data.id, data, { new: true })
+    .lean()
+    .exec()
 }
